dc29d9f7e96fbc91532d659f5a39f51c743a39d8
WaiNaat <qoli0703@gmail.com>
2022-04-11 14:51:13 +0900

    Quantization Results

--- README.md
+++ README.md
@@ -5,11 +5,17 @@
 
 [Mnster00/simplifiedUnetSR](https://github.com/Mnster00/simplifiedUnetSR)
 
+[amirgholami/ZeroQ](https://github.com/amirgholami/ZeroQ)
+
+[xushoukai/GDFQ](https://github.com/xushoukai/GDFQ)
+
 ### Paper
 
 Lu, Z., & Chen, Y. (2021). Single image super-resolution based on a modified U-net with mixed gradient loss. Signal, Image and Video Processing, 1-9.   
 
 Van Der Jeught, S., Muyshondt, P. G., & Lobato, I. (2021). Optimized loss function in deep learning profilometry for improved prediction performance. Journal of Physics: Photonics, 3(2), 024014.   
+
+Xu, S., Li, H., Zhuang, B., Liu, J., Cao, J., Liang, C., & Tan, M. (2020, August). Generative low-bitwidth data free quantization. In European Conference on Computer Vision (pp. 1-17). Springer, Cham.
 
 ### Run
 python main.py -m unet -uf 2 -lr 0.001 -n 300
--- Unet/solver.py
+++ Unet/solver.py
@@ -4,6 +4,7 @@ from math import log10
 
 import torch
 import torch.backends.cudnn as cudnn
+import torch.nn as nn
 
 #from FilterCNN.model import Net
 from progress_bar import progress_bar
@@ -18,6 +19,9 @@ import numpy as np
 import os
 import logging
 import sys
+import copy
+
+from quantization_utils.quant_modules import *
 
 
 class unetTrainer(object):
@@ -25,7 +29,10 @@ class unetTrainer(object):
         super(unetTrainer, self).__init__()
         self.CUDA = torch.cuda.is_available()
         self.device = torch.device('cuda' if self.CUDA else 'cpu')
+
         self.model = None
+        self.model_teacher = None
+
         self.lr = config.lr
         self.nEpochs = config.nEpochs
         self.criterion = None
@@ -61,11 +68,15 @@ class unetTrainer(object):
         #self.model = Net(num_channels=3, base_filter=64, upscale_factor=self.upscale_factor).to(self.device)
         if self.upscale_factor==2:
             self.model=UNet2(3,3).to(self.device)
+            self.model_teacher=UNet2(3,3).to(self.device)
         if self.upscale_factor==4:
             self.model=UNet4(3,3).to(self.device)
+            self.model_teacher=UNet4(3,3).to(self.device)
         if self.upscale_factor==8:
             self.model=UNet8(3,3).to(self.device)
+            self.model_teacher=UNet8(3,3).to(self.device)
         self.model.weight_init(mean=0.0, std=0.01)
+        self.model_teacher.weight_init(mean=0.0, std=0.01)
         self.criterion = torch.nn.MSELoss()
         self.criterion_3=torch.nn.L1Loss()
         self.criterion_2 = GradientLoss()
@@ -87,13 +98,75 @@ class unetTrainer(object):
         torch.save(self.model.state_dict(), os.path.join(self.save_path, model_name))
         print("Model saved.")
 
+    def quantize_model(self, model):
+        """
+        Recursively quantize a pretrained single-precision model to int8 quantized model
+        model: pretrained single-precision model
+        """
+        
+        weight_bit = 8
+        act_bit = 8
+        
+        # quantize convolutional and linear layers
+        if type(model) == nn.Conv2d:
+            quant_mod = Quant_Conv2d(weight_bit=weight_bit)
+            quant_mod.set_param(model)
+            return quant_mod
+        elif type(model) == nn.Linear:
+            quant_mod = Quant_Linear(weight_bit=weight_bit)
+            quant_mod.set_param(model)
+            return quant_mod
+        elif type(model) == nn.ConvTranspose2d:
+            quant_mod = Quant_ConvTranspose2d(weight_bit=weight_bit)
+            quant_mod.set_param(model)
+            return quant_mod
+        
+        # quantize all the activation
+        elif type(model) == nn.ReLU or type(model) == nn.ReLU6:
+            return nn.Sequential(*[model, QuantAct(activation_bit=act_bit)])
+        
+        # recursively use the quantized module to replace the single-precision module
+        elif type(model) == nn.Sequential:
+            mods = []
+            for n, m in model.named_children():
+                mods.append(self.quantize_model(m))
+            return nn.Sequential(*mods)
+        else:
+            q_model = copy.deepcopy(model)
+            for attr in dir(model):
+                mod = getattr(model, attr)
+                if isinstance(mod, nn.Module) and 'norm' not in attr:
+                    setattr(q_model, attr, self.quantize_model(mod))
+            return q_model
+    
     def train(self):
         self.model.train()
+        #self.model_teacher.eval()
         train_loss = 0
         for batch_num, (data, target) in enumerate(self.training_loader):
 
             data, target = data.to(self.device), target.to(self.device)
+            #data = data.to(self.device)
+
             self.optimizer.zero_grad()
+
+            '''
+            # predict
+            prediction = self.model(data)
+            prediction_teacher = self.model_teacher(data)
+
+            # calculate kd loss(L1)
+            loss = self.criterion_3(prediction, prediction_teacher)
+
+            # MSE loss
+            #loss = self.criterion(prediction, prediction_teacher)
+
+            # MixGE loss
+            #mseLoss = self.criterion(prediction, prediction_teacher)
+            #geLoss = self.criterion_2(prediction, prediction_teacher)
+            #loss = mseLoss + 0.1 * geLoss
+            '''
+
             prediction = self.model(data)
 
             # L1 loss
@@ -115,6 +188,7 @@ class unetTrainer(object):
 
             #print(str(loss1.cpu().detach().numpy())+'  '+str(loss_ssim.cpu().detach().numpy()))
             
+
             train_loss += loss.item()
             loss.backward()
             self.optimizer.step()
@@ -155,9 +229,48 @@ class unetTrainer(object):
         avg_ssim /= len(self.testing_loader)
         self.logger.info(f"Testing:  Average PSNR: {avg_psnr:.4f}  SSIM: {avg_ssim:.4f}")
 
+    def test_teacher(self):
+        self.model_teacher.eval()
+        avg_psnr = 0
+        avg_ssim = 0
+        with torch.no_grad():
+            for batch_num, (data, target) in enumerate(self.testing_loader):
+                data, target = data.to(self.device), target.to(self.device)
+
+                prediction = self.model_teacher(data)
+
+                mse = self.criterion(prediction, target)
+
+                psnr = 10 * log10(1 / mse.item())
+                avg_psnr += psnr
+                ssim_value = ssim(prediction, target, data_range=1)
+                
+                avg_ssim += ssim_value
+                #progress_bar(batch_num, len(self.testing_loader), 'PSNR: %.4f | SSIM: %.4f' % ((avg_psnr / (batch_num + 1)),avg_ssim / (batch_num + 1)))
+
+        avg_psnr /= len(self.testing_loader)
+        avg_ssim /= len(self.testing_loader)
+        self.logger.info(f"\nTesting Teacher Model:  Average PSNR: {avg_psnr:.4f}  SSIM: {avg_ssim:.4f}\n")
+
     def run(self):
+        # load model and 
         self.build_model()
+
+        trained_model_path = "result_BSD300_mixGE/my_model.pth"
+
+        self.model_teacher.load_state_dict(torch.load(trained_model_path))
+        self.model_teacher.to(self.device)
+
+        self.model.load_state_dict(torch.load(trained_model_path))
+        self.model = self.quantize_model(self.model)
+        self.model.to(self.device)
+
         self.logger.info(self.model)
+
+        # test teacher model
+        self.test_teacher()
+
+        # train
         for epoch in range(1, self.nEpochs + 1):
             self.logger.info("\n===> Epoch {} starts:".format(epoch))
             self.train()
